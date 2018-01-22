101b66a1faaa5357004022650110ad0a303827d9
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-01-22 16:12:31 +0530

    vari.c

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int c,i,n;
+printf("enter the number:");
+scanf("%d",&n);
+if((i>=1&&i<=n)||(c>=a&&c<=b))
+{
+printf("%d is an consant",n);
+else
+{
+printf("%c is an variable",c);
+return o;
+}
