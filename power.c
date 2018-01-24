3b5d7827915d064687ca931201d87a8cf477d9a1
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-01-24 16:35:04 +0530

    power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int base,exp;
+int result=1;
+printf("enter the base number:");
+scanf("%d",&base);
+printf("the exp number:");
+scanf("%d',&exp);
+while(exp!=0);
+result *=base;
+--exp;
+printf("answer is %d",result);
+return 0;
+}
