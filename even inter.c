519b8a24406ec5a5db335df8dd38dd5f833391ca
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-01-31 16:54:56 +0530

    even inter.c

---
+++ even inter.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,rem,i;
+printf("enter the  first number");
+scanf("%d",&n1);
+printf("enter the  second number");
+scanf("%d",&n2);
+printf("the even number between %dand%d:",n1,n2);
+if(i=n1;i<=n2;++i){
+rem=i%2;
+if(rem==0)
+printf("\n %d",i);
+}
+return 0;
+}
