61a6d71202a1e4b30f9a65f770535f9fb1788ac1
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-01-31 16:58:58 +0530

    armm,c

---
+++ armm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum,cube;
+sum=a*100+b*10+c*1;
+cube=a*a*a+b*b*b+c*c*c;
+printf("enter the number;");
+scanf("%d%d",&cube,&sum);
+if(sum==cube)
+{printf("it is armsrtong");
+else
+printf("it is not armstrong");
+}
+return 0;
+}
+
