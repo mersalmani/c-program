886a152403cee9dbbaf6434a06bd899dfc44f805
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-02-02 16:59:10 +0530

    max.c

---
+++ max.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int [a][b][c],n;
+printf("enter the number is")
+scanf("%d",&n);
+if(i=0;i<n;i++)
+printf("the numbers are%d");
+
+if(a>b||a>c&&b>c||c>a){
+printf("this is biggest in number");
+else
+{
+printf("no");
+}
+}return 0;
+}
+
