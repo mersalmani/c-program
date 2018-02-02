79a5bac637f1535a2f73f754cfe43ab953e620a3
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-02-02 16:26:16 +0530

    diff.c

---
+++ diff.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum,cube=0,i,n;
+sum=a*100+b*10+c*1;
+cube=a*a*a+b*b*b+c*c*c;
+printf("\n enter the number bw %d and %d is");
+scanf("%d%d",&n);
+if(i=0;i<n;++i);
+if(cube==sum)
+{
+printf("yes");
+else
+printf("no");
+}
+return0;
+}
