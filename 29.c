b00424ea7aa21bf2e28a96f31e3b1ca9be3f86bd
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-02-07 16:26:48 +0530

    29.c

---
+++ 29.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,hour,minute;
+printf("enter the time is:");
+scanf("%d%d",&minute,&hour);
+a=hour/60;
+b=minute/60;
+time=minute%10;
+printf("the times are ");
+return 0;
+}
