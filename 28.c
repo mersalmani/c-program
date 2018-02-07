4d01395b2abbc152d0cbd89fcda9ed3e3fdf351b
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-02-07 16:17:50 +0530

    28.c

---
+++ 28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,n,a[7];
+printf("enter the limits");
+scanf("%d",&n);
+print("\n enter the elements are:");
+if(a==0)
+{
+scanf("%d",&a[i]);
+printf("%d %d",a[i],i);
+}
+return 0;
+}
