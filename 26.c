ccb69953a2a1d45ed4b82b6abc5cb63c6453bc3f
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-02-09 15:48:08 +0530

    26.c

---
+++ 26.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a[50],b[50];
+int i;
+void merge(int low,int mid,int high)
+int n;
+if(a==b)
+{
+printf("\nenter the element are:");
+scanf("%d",&n);
+if(low<high)
+{
+mid=(low+high)/2;
+sort(low,mid);
+sort(mid+1,high);
+merge(low,mid,high);
+else
+return;
+}}
+return 0;
+
