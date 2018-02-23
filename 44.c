bbf74a6a13352bd800baf123204804ce5075374e
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-02-23 16:29:46 +0530

    44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the range");
+scanf("%d",&a);
+if((a>1)&&(a<10))
+
+printf("yes");
+else
+
+printf("no");
+
+getch();
+}
