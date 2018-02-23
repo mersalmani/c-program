d1dbe2d13f45a1a79b8937bd3a871746a9bff18a
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-02-23 15:57:41 +0530

    43.c

---
+++ 43.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+ void main()
+{
+char s1[40];
+char s2[40];
+printf("enter the firststring\n");
+gets(s1);
+printf("enter the secondsring\n");
+gets(s2);
+strcat(s1,s2);
+
+printf("%s\n",s1);
+getch)(;
+}
+
