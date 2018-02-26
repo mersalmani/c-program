ed6c7af1243771bf554e3700792172454a0059be
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-02-26 16:31:21 +0530

    52.c

---
+++ 52.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ch;
+clrscr();
+printf("enter the number");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1: printf("\n one");
+break;
+case 2:printf("\n two");
+break;
+case 3:printf("\n three");
+break;
+case 4:
+printf("\n four");
+break;
+case 5:
+printf("\n five");
+break;
+case 6:
+printf("\n six");
+break;
+case 7:
+printf("\n seven");
+break;
+case 8:
+printf("\n eight");
+break;
+case 9:
+printf("\n nine");
+break;
+case 10:
+printf("\n ten");
+break;
+default:
+print("invalid");
+}
+getch();
+}
