afc1a8a0cbd8467448b3fe22e907fea573209e8b
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-03-05 16:26:04 +0530

    67.c

---
+++ 67.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+void  main() 
+
+{
+	int n;
+	scanf("%d",&n);
+  if(n<=10000)
+  {
+	while(n%10!=0)
+
+	{
+
+		n=n+1;
+
+	}}
+
+	printf("\n%d",n);
+getch();
+}
