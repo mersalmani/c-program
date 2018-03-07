62e646019abe92caf439c1c726aa0ab2dd556f23
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-03-07 16:44:02 +0530

    63.c

---
+++ 63.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+char s[50];
+int count=0;
+scanf("%s",s);
+
+for(i=0;i<s[i]!='\0';i++)
+{
+
+
+if(s[i]=='')
+{
+count++;
+}}
+printf("%d",count+1);
+getch();
+}
+
