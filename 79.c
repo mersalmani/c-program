c5e6d96037b6b2a9c4bbf1369b80b90d43f5237a
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-03-12 16:54:46 +0530

    79.c

---
+++ 79.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,i,sq,re,temp;
+scanf("%d%d",&n1,&n2);
+re=n1*n2;
+for(i-0;i<=10000;i++)
+{sq=i*i;
+if(re==sq)
+{
+temp=1;
+}}
+if(temp==1)
+printf("yes");
+else
+printf("no");
+getch();
+}
