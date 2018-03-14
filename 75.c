4db0e0cc9d98c053610fe9281a77165de6a479f2
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-03-14 16:35:24 +0530

    75.c

---
+++ 75.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+
+{
+char s[50];
+int i,l,n;
+scanf("%s",s);
+l=strlen(s);
+for(i=0;i<=n;i++)
+{
+if(n==2)
+{
+printf("*");
+getch();
+}
