bb3e888c8650b7e1ecd061626086c9fb7a229581
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-03-21 15:57:07 +0530

    88.c

---
+++ 88.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,min,l;
+scanf("%d",&a,&b);
+min=(a<b)?a:b;
+for(min=2;min<=a&&min<<b;++m)
+{
+if(a%min==0)&&(b%min==0)
+{
+break;
+}
+}
+l=(a+b)/min;
+getch();
+}
