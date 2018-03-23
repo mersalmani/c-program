e96ca3ce3f8337013c8194d8bb61163e4640e85b
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-03-23 16:46:55 +0530

    gm.c

---
+++ gm.c
@@ -0,0 +1,19 @@
+#ibnclude<stdio.h>
+#include<conio.h>
+void main()
+{
+int n[10];
+scanf("%d",&n[10]);
+for(i=0;i<=2;i++)
+{
+if(n==0)
+{
+printf("yes");
+else
+{
+printf("no");
+}
+}
+}
+getch();
+}
