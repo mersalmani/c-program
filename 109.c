67c30255c9c88fdeb870da7aae7a5f72bfccfb6c
manikandan.M <30549798+mersalmani@users.noreply.github.com>
2018-03-26 16:34:19 +0530

    109.c

---
+++ 109.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s[30],i,min;
+for(i=1;i<=10;i++)
+{
+scanf("%d",s[i]);
+}
+min=a[0];
+
+for(i=1;i<=10;i++)
+{
+if(min>s[i])
+{
+min=s[i];
+}}
+pritf("%d",min);
+getch();
+}
